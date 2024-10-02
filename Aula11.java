f8af5d3f1fcc3f7c4a403e60d144f1cf0302cdd2
higorsenacosa <110289392+higorsenacosa@users.noreply.github.com>
2024-10-02 15:11:30 -0300

    Create Aula11.java

---
+++ Aula11.java
@@ -0,0 +1,7 @@
+public class Aula11 {
+    public static void main(String[] args) {
+        FabricaMoto moto = new FabricaMoto();
+        System.out.println(moto.nome);
+        moto.andar();
+    }
+}
