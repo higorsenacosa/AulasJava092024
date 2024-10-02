3e25fde7eae60bfb1d35863ed732db28833ce9a5
higorsenacosa <110289392+higorsenacosa@users.noreply.github.com>
2024-10-02 15:09:17 -0300

    Create Aula9.java

---
+++ public class Aula9 {     public static void main(String[] args){         Object[][] dados = new Object[30][9];         dados[0][0] = "Higor";         dados[0][1] = "Morais";         dados[0][2] = 'M';         dados[0][3] = "21/07/Aula9.java
@@ -0,0 +1,10 @@
+public class Aula9 {
+    public static void main(String[] args){
+        Object[][] dados = new Object[30][9];
+        dados[0][0] = "Higor";
+        dados[0][1] = "Morais";
+        dados[0][2] = 'M';
+        dados[0][3] = "21/07/1996";
+        System.out.println(dados[0][3] + " - " + dados[0][2]);                
+    }
+}
